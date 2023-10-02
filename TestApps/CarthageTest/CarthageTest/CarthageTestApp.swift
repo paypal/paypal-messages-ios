b21c48e7c2978838f04c5d8ec511db0da5032c77
Nathan Schott <nschott@paypal.com>
2023-10-02 14:25:40 -0400

    Carthage test app

---
+++ TestApps/CarthageTest/CarthageTest/CarthageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarthageTestApp.swift
+//  CarthageTest
+//
+//  Created by Nathaniel Schott on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CarthageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestApps/CarthageTest/CarthageTest/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  CarthageTest
+//
+//  Created by Nathaniel Schott on 9/29/23.
+//
+
+import SwiftUI
+import PayPalMessages
+
+struct ContentView: View {
+    var message = PayPalMessageView(
+        config: .init(
+            data: .init(
+                clientID: "test_app",
+                environment: .sandbox
+            )
+        )
+    )
+
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
