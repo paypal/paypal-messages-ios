94ebcf2d5a58d72d4a435101fbc113bddd50bc8b
Surekha Weinberg <suweinberg@paypal.com>
2024-05-14 13:23:32 -0700

    fix constraint violations

--- Sources/PayPalMessages/PayPalMessageModalViewModel.swift
+++ Sources/PayPalMessages/PayPalMessageModalViewModel.swift
@@ -1,5 +1,37 @@
 import WebKit
 
+fileprivate final class InputAccessoryHackHelper: NSObject {
+    @objc var inputAccessoryView: AnyObject? { return nil }
+}
+
+extension WKWebView {
+    func hack_removeInputAccessory() {
+        print("s")
+        guard let target = scrollView.subviews.first(where: {
+            String(describing: type(of: $0)).hasPrefix("WKContent")
+        }), let superclass = target.superclass else {
+            return
+        }
+
+        let noInputAccessoryViewClassName = "\(superclass)_NoInputAccessoryView"
+        var newClass: AnyClass? = NSClassFromString(noInputAccessoryViewClassName)
+
+        if newClass == nil, let targetClass = object_getClass(target), let classNameCString = noInputAccessoryViewClassName.cString(using: .ascii) {
+            newClass = objc_allocateClassPair(targetClass, classNameCString, 0)
+
+            if let newClass = newClass {
+                objc_registerClassPair(newClass)
+            }
+        }
+
+        guard let noInputAccessoryClass = newClass, let originalMethod = class_getInstanceMethod(InputAccessoryHackHelper.self, #selector(getter: InputAccessoryHackHelper.inputAccessoryView)) else {
+            return
+        }
+        class_addMethod(noInputAccessoryClass.self, #selector(getter: InputAccessoryHackHelper.inputAccessoryView), method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+        object_setClass(target, noInputAccessoryClass)
+    }
+}
+
 class PayPalMessageModalViewModel: NSObject, WKNavigationDelegate, WKScriptMessageHandler {
 
     // MARK: - Properties
@@ -129,6 +161,8 @@ class PayPalMessageModalViewModel: NSObject, WKNavigationDelegate, WKScriptMessa
 
         super.init()
 
+        // Used to remove input accessory bar (next/previous/Done)
+        webView.hack_removeInputAccessory()
         // Used to hook into navigation lifecycle events
         webView.navigationDelegate = self
         // Used to communicate inside the webview
@@ -314,3 +348,4 @@ class PayPalMessageModalViewModel: NSObject, WKNavigationDelegate, WKScriptMessa
         loadCompletionHandler?(.success(()))
     }
 }
+
